875f40dbb0d6b15ea1537dab965a8fb721aaa558
Libern <libernlin@gmail.com>
2016-08-03 00:48:36 +0800

    Improved ValidatesRequests using differently from API or Web

--- src/Someline/Base/Api/Controllers/Controller.php
+++ src/Someline/Base/Api/Controllers/Controller.php
@@ -8,15 +8,13 @@ namespace Someline\Base\Api\Controllers;
 
 
 use Dingo\Api\Routing\Helpers;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesResources;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Someline\Api\Foundation\Validation\ValidatesRequests;
 use Someline\AuthUserHelpers;
 use Someline\Base\Http\Controllers\Controller as BaseController;
 
 abstract class Controller extends BaseController
 {
+    use ValidatesRequests;
     use Helpers;
 
 }
