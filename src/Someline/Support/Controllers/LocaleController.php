2c23afdae1223aa8a72a3a9e9f4006f699c33ebc
Libern <libernlin@gmail.com>
2016-08-13 16:17:38 +0800

    Added LocaleController

---
+++ src/Someline/Support/Controllers/LocaleController.php
@@ -0,0 +1,49 @@
+<?php namespace Someline\Support\Controllers;
+
+/**
+ * Created for someline-starter.
+ * User: Libern
+ */
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Someline\Base\Http\Controllers\Controller;
+
+class LocaleController extends Controller
+{
+
+    /**
+     * @param Request $request
+     * @param $locale
+     * @return \Illuminate\Http\Response
+     */
+    public function getLocaleJs(Request $request, $locale)
+    {
+        $content = 'window.Someline.locales = ' . json_encode(trans('app', [], 'messages', $locale));
+        $response = response()->make($content);
+        $response->header('Content-Type', 'application/javascript');
+        $response->setPublic()
+            ->setMaxAge(604800)
+            ->setExpires(Carbon::now()->addDay(7));
+        return $response;
+    }
+
+    /**
+     * @param Request $request
+     * @param $locale
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function getSwitchLocale(Request $request, $locale)
+    {
+        // check if supported
+        $supportedLanguagesKeys = \LaravelLocalization::getSupportedLanguagesKeys();
+        if (!in_array($locale, $supportedLanguagesKeys)) {
+            abort(404);
+        }
+
+        // store in session
+        session(['someline-locale' => $locale]);
+        return redirect('/');
+    }
+
+}
