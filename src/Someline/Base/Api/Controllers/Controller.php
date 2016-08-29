f59817199207110019ba87a0655af23e60ef9527
Libern <libernlin@gmail.com>
2016-08-30 00:25:15 +0800

    Improved Api Base Controller

--- src/Someline/Base/Api/Controllers/Controller.php
+++ src/Someline/Base/Api/Controllers/Controller.php
@@ -8,13 +8,16 @@ namespace Someline\Base\Api\Controllers;
 
 
 use Dingo\Api\Routing\Helpers;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
 use Someline\Api\Foundation\Validation\ValidatesRequests;
-use Someline\AuthUserHelpers;
-use Someline\Base\Http\Controllers\Controller as BaseController;
+use Someline\Auth\AuthUserHelpers;
 
 abstract class Controller extends BaseController
 {
-    use ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthUserHelpers;
     use Helpers;
 
 }
